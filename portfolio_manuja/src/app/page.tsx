548c9f837ea9a28ea08c58ebf1ea62a53c014890
Manuja Guruge <125580558+manujaguruge123@users.noreply.github.com>
2024-04-02 13:30:05 +0530

    Add profile pic and social media icons

--- portfolio_manuja/src/app/page.tsx
+++ portfolio_manuja/src/app/page.tsx
@@ -26,7 +26,38 @@ export default function Home() {
           </ul>
         </nav>
 
+        <div className="text-center p-10">
+          <h1 className="text-5xl font-bold text-yellow-400 ">Manuja Guruge</h1>
+          <h3 className="text-2xl py-4 md:text-3xl ">Mobile App and Website Developer</h3>
+          <p className="text-sm py-4 text-gray-400 leading-5 max-w-xl mx-auto ">Presently, I'm an undergraduate student at the University of Westminster in Software Engineering and passionate about software development. I'm interested in learning new IT technologies and searching for an internship to enhance my professional network and knowledge.</p>
+        </div>
+
+        {/* Socials */}
+        <div className="cursor-pointer text-5xl flex justify-center gap-16 text-gray-600">
+          <a href="https://github.com/manujaguruge123">
+            <AiFillGithub  />
+          </a>
+          <a href="https://www.linkedin.com/in/manuja-guruge-94a19b276/">
+            <AiFillLinkedin />
+          </a>
+          <a href="https://www.instagram.com/mr._manu_03/">
+            <AiFillInstagram/>
+          </a>
+        </div>
+
+        {/* Profile */}
         
+          <div className="m-auto relative bg-gradient-to-b from-yellow-100 w-80 h-80 p-5 mt-20 rounded-full">
+            <Image
+              src={profileImage}
+              alt="Manuja Guruge"
+              // layout="responsive"
+              // objectFit="cover"
+              className="rounded-full"
+              width={300}
+              height={300}
+            />
+          </div>
         
       </section>
     </main>
