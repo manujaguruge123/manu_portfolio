963f645119c80a655d5cbd05cd99d2ff4db934be
Manuja Guruge <125580558+manujaguruge123@users.noreply.github.com>
2024-04-02 13:17:44 +0530

    Add Poppins font to the project

--- portfolio_manuja/tailwind.config.ts
+++ portfolio_manuja/tailwind.config.ts
@@ -1,6 +1,9 @@
+// tailwind.config.js
+
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,13 +11,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        poppins: ["var(--font-poppins)"],
       },
     },
   },
   plugins: [],
 };
+
 export default config;
